c889ae6d53517c78aa4745716983419284555b7a
Peter Polacik <polacik.p@gmail.com>
2013-01-04 14:06:12 +0100

    Finished instruction list

--- ins.h
+++ ins.h
@@ -38,19 +38,56 @@
 
 /* INSTRUCTION LIST */
 
-/* General Instructions */
+/* General Instructions 
+ *
+ * NOP and END don't take any argument
+ *
+ * DBG_FLAGS specify debug settings for VM (disable/enable, log/output,
+ * verbosity etc.)
+ *
+ * STDCALL_FLAGS specify how standard function call will be performed,
+ * they are not used yet.
+ */
 #define IN_NOP		0x00 /* No operation (no arg) */
 #define IN_END		0x01 /* End execution (no arg) */
-#define IN_DEBUG	0x02 /* Toggle debugging (DBG_FLAG) */
-#define IN_STDCALL	0x03 /* Standard Library Function Call (STDCALL_NO) */
+#define IN_DEBUG	0x02 /* Toggle debugging (DBG_FLAGS) */
+#define IN_STDCALL	0x03 /* Standard Function Call (STDCALL_FLAGS) */
 
-/* Stack instructions */
+/* Stack instructions
+ *
+ * {LOAD,DUP,DROP}_SZ specify number of bytes on which the action will be
+ * performed.
+ *
+ * GET_POSSZ specifies, how big is position argument. IN_GET has one external
+ * argument, POSSZ, and 3 internal arguments:
+ *  - GET_SZ (1 byte)
+ *  - GET_POS ([possz] bytes, supported maximum 8)
+ *  - GET_DATA ([sz] bytes @ stack[pos])
+ */
 #define IN_LOAD		0x10 /* Load onto stack (LOAD_SZ) */
 #define IN_DUP		0x11 /* Duplicate head on stack (DUP_SZ) */
-#define IN_GET		0x12 /* Duplicate X element on stack (GET_FLAGS) */
+#define IN_GET		0x12 /* Duplicate X element on stack (GET_POSSZ) */
 #define IN_DROP		0x13 /* Drop head from stack (DROP_SZ) */
 
-/* Arithmetical and Logical */
+/* Arithmetical and Logical
+ * 
+ * UI - unsigned integer
+ * SI - signed integer
+ * UF - unsigned float
+ * SF - signed float
+ *
+ * {ADD,SUB,MUL,DIV,MOD}_SZ determine how many bytes wide are both arguments,
+ * allowed values are: 1, 2, 4, 8 ... depending on functionality of VM
+ *
+ * {ADD,SUB,MUL,DIV}_TYPE determine type of FP arguments available (and thus,
+ * their size (float, double ...)
+ *
+ * {AND,OR,XOR,NOT,SHL,SHR,ROTL,ROTR}_SZ specify width of argument(s) to be
+ * used in these functions. Shift and rotate instructions also have 1-byte
+ * wide argument preceeding the actual number. This argument specifies shift
+ * or rotation space (how many bits to the right/left should the value be
+ * shifted/rotated). This allows for maximum of 255-bit shift/rotation.
+ */
 #define IN_ADD_UI	0x20 /* Add 2 unsigned integers (ADD_SZ) */
 #define IN_ADD_SI	0x21 /* Add 2 signed integers (ADD_SZ) */
 #define IN_ADD_UF	0x22 /* Add 2 unsigned floats (ADD_TYPE) */
@@ -87,11 +124,36 @@
 #define IN_ROTL		0x3C /* Bitwise Rotate Left (ROTL_SZ); max. 255 */
 #define IN_ROTR		0x3D /* Bitwise Rotate Right (ROTR_SZ); max. 255 */
 
-/* Jumps and branching */
+/* Jumps and branching
+ *
+ * {CALL,JMP}_TYPE determine, whether the jump is relative (that is, argument
+ * is 1 32-bit number from stack, signed integer determining how much the addr
+ * will change (+/-)) or absolute (32-bit unsigned integer, address in current
+ * object).
+ *
+ * {CALL,JMP}_FLAGS are currently unused
+ *
+ * RET_COUNT specifies, how many levels should return ... return, this can be
+ * used for quick return to top directory, effectively solving exceptions in
+ * low-level programming fashion.
+ */
 #define IN_JMP		0x40 /* Object-wise jump (JMP_TYPE: REL | ABS) */
 #define IN_JMP_L	0x41 /* Long jump (JMP_FLAGS) */
 #define IN_CALL		0x42 /* Object-wise returnable jump (CALL_TYPE) */
 #define IN_CALL_L	0x43 /* Long returnable jump (CALL_FLAGS) */
 #define IN_RET		0x44 /* Return (RET_COUNT) */
 
-/* TODO: Conditionals */
+/* Conditionals
+ * 
+ * IN_IF* instructions don't use arguments
+ *
+ * CMP_FLAGS contain information about size of arguments in bytes and type:
+ *  -> INT vs. FP
+ */
+#define IN_CMP		0x50 /* Compare 2 arguments (CMP_FLAGS) */
+#define IN_IFEQ		0x51 /* Execute next IN if last CMP was equal */
+#define IN_IFNEQ	0x52 /* ... if last CMP was NOT equal */
+#define IN_IFGT		0x53 /* ... if first argument > second in last CMP */
+#define IN_IFGE		0x54 /* ... if first arg >= second in last CMP */
+#define IN_IFLT		0x55 /* ... if first arg < second in last CMP */
+#define IN_IFLE		0x56 /* ... if first arg <= second in last CMP */
